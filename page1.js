ddb7b19e3b5298f65a36226cbc6ea0f1e9e6e196
연아윤 <w2306@e-mirim.hs.kr>
2024-06-03 03:47:11 +0900

    feat. 공감 기능 구현

--- page1.js
+++ page1.js
@@ -10,3 +10,19 @@ function toggleMenu() {
         menuIcon.innerHTML = '&#9776;'; // 원래 아이콘
     }
 }
+
+function toggleHeart(image) {
+    const container = image.parentElement; // image-container를 선택
+    const likes = container.querySelector('.likes'); // image-container 내에서 .likes 요소 선택
+    let count = parseInt(likes.innerHTML);
+
+    if (image.src.includes("emheart.png")) {
+        image.src = "fuheart.png";
+        count += 1;
+    } else {
+        image.src = "emheart.png";
+        count -= 1;
+    }
+
+    likes.innerHTML = count;
+}
