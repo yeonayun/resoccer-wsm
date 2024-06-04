3b452443d4bf7e4acd7b6189dca900e3448cc7dd
연아윤 <w2306@e-mirim.hs.kr>
2024-06-04 10:21:26 +0900

    feat.사진업로드

--- page2.js
+++ page2.js
@@ -1,17 +1,28 @@
 const toggleMenu = () => {
-    // HTML 요소를 가져옵니다
     const navToggleDiv = document.querySelector(".nav-toggle");
     const menuUl = document.querySelector(".menu");
     const toggleI = navToggleDiv.querySelector("i");
 
     navToggleDiv.onclick = () => {
-        // 클래스에 show-menu를 추가하거나 제거합니다
         menuUl.classList.toggle("show-menu");
-
-        // 아이콘 클래스를 변경합니다
         toggleI.classList.toggle("bi-list");
         toggleI.classList.toggle("bi-x-lg");
     };
 };
 
 toggleMenu();
+
+const fileInput = document.getElementById("fileInput");
+const uploadedImage = document.getElementById("uploadedImage");
+
+fileInput.addEventListener("change", function() {
+    const file = this.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            uploadedImage.src = e.target.result;
+            uploadedImage.style.display = 'block'; // 업로드된 이미지 보이기
+        };
+        reader.readAsDataURL(file);
+    }
+});
