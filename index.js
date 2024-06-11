6684bace9e8985a99dc70a600b61ef7f6589c81b
연아윤 <w2306@e-mirim.hs.kr>
2024-06-11 14:50:15 +0900

    feat. 웹페이지 완성

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const toggleMenu = () => {
-    // HTML 요소를 가져옵니다
+    // HTML 요소를 가져오기
     const navToggleDiv = document.querySelector(".nav-toggle");
     const menuUl = document.querySelector(".menu");
     const toggleI = navToggleDiv.querySelector("i");
 
     navToggleDiv.onclick = () => {
-        // 클래스에 show-menu를 추가하거나 제거합니다
+        // 클래스에 show-menu를 추가/제거
         menuUl.classList.toggle("show-menu");
 
-        // 아이콘 클래스를 변경합니다
+        // 아이콘 변경
         toggleI.classList.toggle("bi-list");
         toggleI.classList.toggle("bi-x-lg");
     };
